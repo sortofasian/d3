ab9569c2556cbe0b3402cbca0ed018ab308fac3c
sortofasian <charliesyvertsen06@icloud.com>
2022-07-28 13:00:27 -0400

    Finish about section

--- frontend/src/layouts/ImageTextPair.tsx
+++ frontend/src/layouts/ImageTextPair.tsx
@@ -1,5 +1,18 @@
-function ImageTextPair() {
-    return <div>ImageTextPair</div>
+import './ImageTextPair.scss'
+
+import { ReactNode } from 'react'
+
+type Props = {
+    children: ReactNode
+    img: string
+}
+function ImageTextPair({ children, img }: Props) {
+    return (
+        <div className='imgtext'>
+            <div className='imgtext-text'>{children}</div>
+            <img className='imgtext-image' src={img} alt='img' />
+        </div>
+    )
 }
 
 export default ImageTextPair
